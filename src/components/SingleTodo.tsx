267d09e175a86243aab31db6059084e9fb872261
ghanayemomar <o.ghanaiem@student.aaup.edu>
2023-09-09 19:23:11 +0300

    adding the delete and the isDone prop to the todo list.

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -10,17 +10,33 @@ type Props = {
 };
 
 const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
-    <form className="todos__signle">
-      <span className="todos__single--text">{todo.todo}</span>
+    <form className="todos__single">
+      {todo.isDone ? (
+        <s className="todos__single--text">{todo.todo}</s>
+      ) : (
+        <span className="todos__single--text">{todo.todo}</span>
+      )}
       <div>
         <span className="icon">
           <AiFillEdit />
         </span>
-        <span className="icon">
+        <span className="icon" onClick={() => handleDelete(todo.id)}>
           <AiFillDelete />
         </span>
-        <span className="icon">
+        <span className="icon" onClick={() => handleDone(todo.id)}>
           <MdDone />
         </span>
       </div>
